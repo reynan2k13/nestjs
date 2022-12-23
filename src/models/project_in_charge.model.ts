4498d48e9c068e2581ac33980c10a39e6a0d900d
SGPCLOUD\I0W <i0w@innodata.com>
2022-12-23 13:56:09 +0800

    update schema

--- src/models/project_in_charge.model.ts
+++ src/models/project_in_charge.model.ts
@@ -2,13 +2,9 @@ import * as mongoose from 'mongoose';
 
 export const ProjectInChargeSchema = new mongoose.Schema({
   project : {type: String},
-  data : [
-    {
-      username : {type: String},
-      full_name : {type: String},
-      email_address : {type: String},
-    }
-  ]
+  username : {type: String},
+  full_name : {type: String},
+  email_address : {type: String},
 }, {versionKey : false});
 
 export interface ProjectInCharge {
